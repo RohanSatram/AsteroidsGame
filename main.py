926afa9e824145f778b5a30dd3441d463f3bdd56
RohanSatram <satram.rohan@gmail.com>
2024-10-25 02:29:47 +0530

    Main commit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pygame
 from constants import *
+from player import *
+from asteroid import *
+from asteroidfield import *
+import sys
+from shot import *
 
 def main():
     pygame.init()
@@ -7,7 +12,27 @@ def main():
     print("Screen width:",SCREEN_WIDTH)
     print("Screen height:",SCREEN_HEIGHT)
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (updatable, drawable, shots)
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    player = Player(x,y)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    fps = pygame.time.Clock()
+    dt = 0
+
+    asteroidField = AsteroidField()
 
     while True:
         for event in pygame.event.get():
@@ -15,7 +40,24 @@ def main():
                 return
         black = (0,0,0)
         screen.fill(black, rect=None, special_flags=0)
+
+        for method in updatable:
+            method.update(dt)
+        
+        for method in drawable:
+            method.draw(screen)
+
+        for method in asteroids:
+            if method.check_collision(player):
+                print("Game over!")
+                sys.exit()
+            for shot in shots:
+                if method.check_collision(shot):
+                    method.split()
+                    shot.kill()
+
         pygame.display.flip()
+        dt = fps.tick(60)/1000
 
 if __name__ == "__main__":
     main()
